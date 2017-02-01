a1e02011926bf7a94faf2de39081305cedae9653
Alexander Örnberg <alexander.ornberg@gmail.com>
2017-02-01 10:56:32 +0100

    Add files via upload
    Non-existing dataset.p file and typos

--- Builder.py
+++ Builder.py
@@ -1,40 +1,32 @@
-'''
-Written by Lennart Alexander Ornberg
-for SCC300 Final Year Project
-'''
-
-import Leap, sys, thread, time, random, math, threading, pickle, tkMessageBox, utils
+import Leap
+import sys, thread, time, random, math, os
+import threading
 from time import sleep
+import pickle
 from Tkinter import *
+import tkMessageBox
+import utils
 
-Sign = utils.Sign #import common Sign class
-dataset = [] #global dataset for writing to file
+Sign = utils.Sign
+dataset = []
 
-
-
-# Dialog for creation of a new sign
-# --------------------------------------------------
 class SetNameDialog:
 	def __init__(self, parent):
 		top = self.top = Toplevel(parent)
 		top.wm_title("New Sign")
-		
-		#For entry of word value
+
 		Label(top, text='Word:').grid(row=0, pady=5)
 		self.myEntryBox = Entry(top)
 		self.myEntryBox.grid(row=0, column=1, columnspan=3)
-		
-		#For selection of sign type
+
 		Label(top, text='Type:').grid(row=1, column=0, pady=5)
 		self.radioButtonValue = IntVar()
 		Radiobutton(top, text="Gesture", variable=self.radioButtonValue, value=0).grid(row=1, column=1, pady=5)
 		Radiobutton(top, text="Fixed", variable=self.radioButtonValue, value=1).grid(row=1, column=2)
 
-		#OK button to return to main screen
 		Button(top, text='OK', command=self.send).grid(row=3, column=1, columnspan=2, pady=2)
 		self.value = ''
 
-	#Upon window closing, store values collected
 	def send(self):
 		if self.radioButtonValue.get() == 0:
 			self.signType = 'Gesture'
@@ -44,21 +36,25 @@ class SetNameDialog:
 		self.top.destroy()
 
 
-# Main application window
-# --------------------------------------------------
 class GUI:
 	def __init__(self, master):
 		self.master = master
-		
 
-		self.controller = Leap.Controller()
-		sleep(0.01) #wait for leap controller to initiate
-		self.vectorBuffer = []
-		self.dataset = pickle.load(open("dataset.p", "rb"))
+		#Not working properly
+		#self.master.bind('<KeyPress-space>', self.handleSpacebar)
+		if os.path.exists('dataset.p'):
+			self.dataset = pickle.load(open("dataset.p", "rb"))
+		else:
+			self.dataset = []
+
 		global dataset
 		dataset = self.dataset
-		
-		#List and scrollbar for Signs
+
+		self.controller = Leap.Controller()
+		self.vectorBuffer = []
+		self.currentSign = None
+
+		#Frame holding lists and scrollbars
 		self.signsScrollbar = Scrollbar(master)
 		self.signsList = Listbox(master, yscrollcommand=self.signsScrollbar.set, exportselection=0)
 		self.signsList.grid(row=0, rowspan=5, column=0, columnspan=2)
@@ -66,112 +62,118 @@ class GUI:
 		self.signsScrollbar.config(command=self.signsList.yview)
 		self.signsScrollbar.grid(row=0, column=2, rowspan=5, sticky=N+S)
 
-		#List and scrollbar for 
 		self.samplesScrollbar = Scrollbar(master)
 		self.samplesList = Listbox(master, yscrollcommand=self.samplesScrollbar.set)
 		self.samplesList.grid(row=0, rowspan=5, column=3, columnspan=2)
 		self.samplesScrollbar.config(command=self.samplesList.yview)
 		self.samplesScrollbar.grid(row=0, column=5, rowspan=5, sticky=N+S)
 
+		self.updateSignsList()
+
 		#Buttons for controlling signs
 		self.deleteSign = Button(master, text="Delete", command=self.deleteSign)
 		self.deleteSign.grid(row=6, column=0)
 		self.createSignButton = Button(master, text="New", command=self.createSign)
 		self.createSignButton.grid(row=6, column=1)
-		
+
 		#Buttons for controlling samples
 		self.deleteSampleButton = Button(master, text="Delete Last", command=self.deleteSample)
 		self.deleteSampleButton.grid(row=6, column=3)
+
 		self.recordButton = Button(master, text="Record")
 		self.recordButton.bind("<Button-1>", self.startRecording)
 		self.recordButton.bind("<ButtonRelease-1>", self.stopRecording)
 		self.recordButton.grid(row=6, column=4)
 
-		self.isRecording = False #Boolean for use by recorder thread
-		self.updateSignsList() #Fill list of Signs
-		self.currentSign = None #Sign selected
+		self.isRecording = False
 
+	#Not working properly (sends sequence of press when it is in fact held down)
+	#def handleSpacebar(self, event):
+	#	if self.isRecording == False:
+	#		self.startRecording(None)
+	#	else:
+	#		self.stopRecording(None)
 
 	def updateSignsList(self):
+		# Delete existing entries in list
 		self.signsList.delete(0, END)
+
+		# Fill list alphabetically
 		self.dataset = sorted(self.dataset, key=lambda x : x.value)
 		for sign in self.dataset: #List in alphabetical order
-			self.signsList.insert(END, ' '+sign.value)
+			self.signsList.insert(END, ' ' + sign.value)
 
 	def updateSamplesList(self):
 		if self.currentSign != None:
 			self.samplesList.delete(0, END)
-			for i in range(len(self.currentSign.samples)):
+			for i in range(len(self.currentSign.samples)): #Todo, if empty
 				self.samplesList.insert(END, ' '+str(i+1))
 			self.samplesList.yview(END)
-		else:
-			self.samplesList.delete(0, END)
 
 	def startRecording(self, event):
 		self.vectorBuffer = []
 		if self.controller.is_connected:
-			if len(self.controller.frame().hands) != 0:
-				if self.currentSign != None:
-					self.isRecording = True
-					thread = threading.Thread(target=self._record) #start recording
-					thread.start()
-				else:
-					tkMessageBox.showwarning('','No sign selected')
+			if self.currentSign != None:
+				self.isRecording = True
+				thread = threading.Thread(target=self._record)
+				thread.start()
 			else:
-				tkMessageBox.showwarning('','No hand in view')
+				tkMessageBox.showwarning('','No sign selected')
 		else:
 			tkMessageBox.showwarning('','Leap Motion sensor not connected')
- 
+
 	def _record(self):
 		while self.isRecording:
-			if len(self.controller.frame().hands) != 0:
-				hand = self.controller.frame().hands[0] #todo failure detection here
-				self.vectorBuffer.append(hand)
-				sleep(0.01)
+			hand = self.controller.frame().hands[0] #TODO failure detection
+			self.vectorBuffer.append(hand)
+			sleep(0.01)
+
+	# Not in use, better to let user specify if sign is fixed or a gesture,
+	# instead of detecting it automatically
+	def isGesture(self, _buffer):
+		gesture = 0
+		fixed = 0
+		for i in range(10,len(_buffer)+1,10):
+			if utils.moving(_buffer[i-10],_buffer[i-1]):
+				gesture += 1
 			else:
-				self.isRecording = False
-				tkMessageBox.showwarning('','No hand in view')
+				fixed -= 0
+		return gesture > fixed
 
 
 	def stopRecording(self, event):
 		if self.isRecording:
 			value = self.currentSign
-			self.isRecording = False #kill recording thread
+			self.isRecording = False
 
-			if self.currentSign.type == 'Fixed' and len(self.vectorBuffer):
+			if self.currentSign.type == 'Fixed':
 				self.currentSign.samples.append(utils.vectorize(self.vectorBuffer, 1))
-			
-			elif len(self.vectorBuffer) >= 10: #make enough frames for 10 keyframes
+			elif len(self.vectorBuffer) >= 10: #TODO rename vectorbuffer to framebuffer
 				self.currentSign.samples.append(utils.vectorize(self.vectorBuffer, 10))
-			
-			else:
-				tkMessageBox.showwarning('','No frames recorded')	
 
 			global dataset
-			dataset = self.dataset #update global dataset for storing upon exit
+			dataset = self.dataset
 			self.updateSamplesList()
 
 	def createSign(self):
-		inputDialog = SetNameDialog(self.master) #open dialog to set sign name
+		inputDialog = SetNameDialog(self.master)
 		self.master.wait_window(inputDialog.top)
-    	
-		value = inputDialog.value #assure sign has name and is unique
+
+		value = inputDialog.value
+		# Add sign to list if name has lenght and is not equal to existing signs
 		if len(value) > 0 and sum([1 for x in self.dataset if x.value == value]) == 0:
-			self.currentSign = Sign(value, inputDialog.signType, [])  #create Sign object
+			self.currentSign = Sign(value, inputDialog.signType, [])
 			self.dataset.append(self.currentSign)
 		self.updateSignsList()
-		
-	
+
+
 	def deleteSign(self):
 		selected = self.signsList.curselection()
 		if len(selected) > 0:
 			if tkMessageBox.askyesno("Delete", "Are you sure you want to delete this sign?"):
-				self.currentSign = None
 				del self.dataset[selected[0]]
 				self.updateSignsList()
-				self.updateSamplesList()
-
-				global dataset #Update global dataset to save change on program exit
+				global dataset
 				dataset = self.dataset
 		else:
 			tkMessageBox.showwarning('','No sign selected')
@@ -195,9 +197,8 @@ class GUI:
 		self.updateSamplesList()
 
 
-		
-# Initiate main application window
-# --------------------------------------------------
+
+
 root = Tk()
 root.resizable(width=FALSE, height=FALSE)
 root.wm_title("Builder")
@@ -206,5 +207,3 @@ root.mainloop()
 
 
 pickle.dump(dataset, open("dataset.p", "wb"))
-
-
